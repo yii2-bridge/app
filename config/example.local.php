4094f7e1ad448a39f7557367787fa155a9c282e1
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2018-01-03 13:43:36 +0600

    included example local.php file with gii configured

---
+++ config/example.local.php
@@ -0,0 +1,41 @@
+<?php
+$params = require __DIR__ . '/params.php';
+
+$db = [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2bridge_stage',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
+
+return [
+    'yiiDebug' => true,
+    'yiiEnv' => 'stage',
+    'web' => [
+        'components' => [
+            'log' => [
+                'traceLevel' => 3,
+                'targets' => [
+                    [
+                        'class' => 'yii\log\FileTarget',
+                        'levels' => ['error', 'warning'],
+                    ],
+                ],
+            ],
+            'db' => $db,
+        ],
+        'modules' => [
+            'gii' => [
+                'class' => \yii\gii\Module::className()
+            ]
+        ],
+        'params' => $params,
+    ],
+    'console' => [
+        'components' => [
+            'db' => $db,
+        ],
+        'params' => $params,
+    ]
+];
