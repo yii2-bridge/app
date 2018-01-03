2d3c22635f8c5c0df7998140cae90072406d75f4
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2018-01-03 14:04:39 +0600

    Added mode.php file support to change environment mode

--- web/index.php
+++ web/index.php
@@ -1,6 +1,12 @@
 <?php
 require(dirname(__DIR__) . '/vendor/autoload.php');
 
-$env = new \janisto\environment\Environment(dirname(__DIR__) . '/config');
+$modeFile = dirname(__DIR__) . '/config/mode.php';
+$mode = 'prod';
+if (file_exists($modeFile)) {
+    $mode = file_get_contents($modeFile);
+}
+
+$env = new \janisto\environment\Environment(dirname(__DIR__) . '/config', $mode);
 $env->setup();
 (new yii\web\Application($env->web))->run();
