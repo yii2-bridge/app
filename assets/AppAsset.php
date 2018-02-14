767550472c4b0e8a8aeea4a5372d97740fa47225
naffiq <naffiq@gmail.com>
2018-02-14 22:58:56 +0600

    Added laravel-mix for scss and es6 features

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,9 +20,10 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/main.css',
     ];
     public $js = [
+        'js/main.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
