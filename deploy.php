9825339bfaa68d42ac96ae46eb392919b0b25eeb
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2018-03-19 17:00:15 +0600

    Updated config for gitlab-ci deployer setup

--- deploy.php
+++ deploy.php
@@ -6,25 +6,53 @@ require 'recipe/yii2-app-basic.php';
 // Configuration
 
 set('repository', 'git@github.com:naffiq/yii2-app-bridge.git');
-set('git_tty', true); // [Optional] Allocate tty for git on first deployment
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
+
+// Shared files/dirs between deploys
+add('shared_files', ['config/local.php']);
+add('shared_dirs', ['web/media', 'runtime']);
+
+// Writable dirs by web server
+add('writable_dirs', ['web/media', 'runtime']);
+
 set('allow_anonymous_stats', false);
+
+
+
+/** @var $userProduction    string username for production host    */
+$userProduction = getenv('USER_PRODUCTION') ?: 'admin';
+/** @var $userStage         string username for stage host         */
+$userStage = getenv('USER_STAGE') ?: 'admin';
 
 // Hosts
 
 host('project.com')
     ->stage('production')
+    ->user($userProduction)
     ->set('deploy_path', '/var/www/project.com');
     
-host('beta.project.com')
-    ->stage('beta')
+host('demo.project.com')
+    ->stage('stage')
+    ->user($userStage)
     ->set('deploy_path', '/var/www/project.com');  
 
 
 // Tasks
 
+task('deploy:config', function () {
+    $stage = get('stage');
+    run("echo '{$stage}' > {{release_path}}/config/mode.php");
+    run("cat {{release_path}}/config/mode.php");
+})->desc('Set application stage config');
+after('deploy:update_code', 'deploy:config');
+
+task('deploy:run_migrations', function () {
+    run('cd {{release_path}} && ./vendor/bin/bridge-install');
+    // Add your migrations here ...
+})->desc('Run migrations');
+
+/*
+ * Uncomment code below if OPCache is on for php-fpm
+ *
 desc('Restart PHP-FPM service');
 task('php-fpm:restart', function () {
     // The user must have rights for restart service
@@ -32,6 +60,7 @@ task('php-fpm:restart', function () {
     run('sudo systemctl restart php-fpm.service');
 });
 after('deploy:symlink', 'php-fpm:restart');
+*/
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ require(dirname(__DIR__) . '/vendor/autoload.php');
 $modeFile = dirname(__DIR__) . '/config/mode.php';
 $mode = 'prod';
 if (file_exists($modeFile)) {
-    $mode = file_get_contents($modeFile);
+    $mode = trim(file_get_contents($modeFile));
 }
 
 $env = new \janisto\environment\Environment(dirname(__DIR__) . '/config', $mode);
