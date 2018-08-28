8b47a0ace1b7783add3a326e99e43b318315d2b4
altynbek <altinbek__97@mail.ru>
2018-08-28 11:16:29 +0600

    fix: default charset DB is utf8mb4

--- config/example.local.php
+++ config/example.local.php
@@ -6,7 +6,7 @@ $db = [
     'dsn' => 'mysql:host=localhost;dbname=yii2bridge_local',
     'username' => 'root',
     'password' => '',
-    'charset' => 'utf8',
+    'charset' => 'utf8mb4',
 ];
 
 return [
--- config/mode_test.php
+++ config/mode_test.php
@@ -6,7 +6,7 @@ $db = [
     'dsn' => 'mysql:host=localhost;dbname=yii2bridge_test',
     'username' => 'root',
     'password' => '',
-    'charset' => 'utf8',
+    'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
     'enableSchemaCache' => true,
